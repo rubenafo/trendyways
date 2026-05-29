335cf84a82026844797aa2c2e87291d022eb4bbe
Ruben <rbfrancos@gmail.com>
2026-05-29 08:26:01 +0200

    Add sample page

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// Main entry point for trendyways library
+module.exports = {
+  // Vectors
+  ...require('./vectors'),
+  
+  // Averages
+  ...require('./averages'),
+  
+  // Indicators
+  ...require('./indicators'),
+  
+  // Statistics
+  ...require('./statistics'),
+  
+  // ADX
+  ...require('./adx'),
+  
+  // Bollinger
+  ...require('./bollinger'),
+  
+  // Support & Resistance
+  ...require('./sup_res'),
+  
+  // Utils
+  ...require('./utils')
+};
